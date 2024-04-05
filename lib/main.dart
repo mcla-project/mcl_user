d4be275962d213867dd7fc4b005b1f1489de421c
mebcrtz <81579471+mebcrtz@users.noreply.github.com>
2024-04-05 15:30:24 +0800

    [build] setup firebase for ios, android and web

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import './components/base_layout.dart';
 import './pages/splash_screen/splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+      apiKey: "AIzaSyD_4uLrDvoiPhfPK8aw4QtwlwxCA6mAFls",
+      appId: "1:815166244171:web:f523c6b7f9b89ab88b14ce",
+      messagingSenderId: "815166244171",
+      projectId: "mcla-51bb1",
+    ));
+  }
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
@@ -22,10 +36,10 @@ class _MyAppState extends State<MyApp> {
         canvasColor: const Color(0xFF013822),
       ),
       title: 'Manila City Library',
-      initialRoute: '/', 
+      initialRoute: '/',
       routes: {
-        '/': (context) => const AnimatedSplashScreen(), 
-        '/base_layout': (context) => const BaseLayout(), 
+        '/': (context) => const AnimatedSplashScreen(),
+        '/base_layout': (context) => const BaseLayout(),
       },
     );
   }
