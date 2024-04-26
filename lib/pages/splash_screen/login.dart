a07be4f3cfa5237a4b44a3125c739dea3f07a50e
mebcrtz <81579471+mebcrtz@users.noreply.github.com>
2024-04-27 02:07:18 +0800

    [feature] login auth routing fixed

--- lib/pages/splash_screen/login.dart
+++ lib/pages/splash_screen/login.dart
@@ -1,99 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import '../../components/base_layout.dart';
-import 'signup.dart';
 
-class FirstLoginScreen extends StatelessWidget {
-  const FirstLoginScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 12.0),
-                  child: Image.asset(
-                    'images/mnlcitylib_logo.png',
-                    width: 200,
-                    height: 200,
-                  ),
-                ),
-                Container(
-                  padding: const EdgeInsets.all(20),
-                  child: Column(
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const NextLoginScreen()),
-                          );
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors
-                              .green.shade900, // Set the button color to green
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(
-                                10), // Set the corner radius
-                          ),
-                        ),
-                        child: const Text(
-                          'Login',
-                          style: TextStyle(fontSize: 25, color: Colors.white),
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const SignUpScreen()),
-                          );
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors
-                              .green.shade900, // Set the button color to green
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(
-                                10), // Set the corner radius
-                          ),
-                        ),
-                        child: const Text(
-                          'Sign Up',
-                          style: TextStyle(fontSize: 25, color: Colors.white),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class NextLoginScreen extends StatefulWidget {
-  const NextLoginScreen({Key? key}) : super(key: key);
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
 
   @override
-  NextLoginScreenState createState() => NextLoginScreenState();
+  LoginScreenState createState() => LoginScreenState();
 }
 
-class NextLoginScreenState extends State<NextLoginScreen> {
+class LoginScreenState extends State<LoginScreen> {
   bool _obscureText = true;
 
   final TextEditingController _emailController = TextEditingController();
@@ -121,14 +37,14 @@ class NextLoginScreenState extends State<NextLoginScreen> {
 
       if (userCredential.user != null) {
         ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
+          const SnackBar(
             content: Text("User is successfully signed in"),
           ),
         );
-        Navigator.pushNamed(context, "/home");
+        Navigator.pushNamed(context, "/base_layout");
       } else {
         ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
+          const SnackBar(
             content: Text("Some error occurred"),
           ),
         );
@@ -145,6 +61,23 @@ class NextLoginScreenState extends State<NextLoginScreen> {
       });
     }
   }
+
+  // Future signIn() async {
+  //   try {
+  //   await FirebaseAuth.instance.signInWithEmailAndPassword(
+  //     email: _emailController.text.trim(),
+  //     password: _passwordController.text.trim(),
+  //   );
+  //   }
+
+  // }
+
+@override
+void dispose() {
+  _emailController.dispose();
+  _passwordController.dispose();
+  super.dispose();
+}
 
   @override
   Widget build(BuildContext context) {
@@ -192,7 +125,7 @@ class NextLoginScreenState extends State<NextLoginScreen> {
                 TextFormField(
                   controller: _emailController,
                   decoration: const InputDecoration(
-                    labelText: 'Email',
+                    hintText: 'Email',
                     border: OutlineInputBorder(),
                   ),
                 ),
@@ -201,7 +134,7 @@ class NextLoginScreenState extends State<NextLoginScreen> {
                 TextFormField(
                   controller: _passwordController,
                   decoration: InputDecoration(
-                    labelText: 'Password',
+                    hintText: 'Password',
                     border: const OutlineInputBorder(),
                     suffixIcon: IconButton(
                       icon: Icon(
@@ -218,17 +151,42 @@ class NextLoginScreenState extends State<NextLoginScreen> {
                 ),
                 const SizedBox(height: 20),
                 // Login button
+
+                // Padding(
+                //   padding: const EdgeInsets.symmetric(vertical: 15),
+                //   child: Container(
+                //     padding: const EdgeInsets.all(20),
+                //     decoration: BoxDecoration(
+                //       color: Colors.green.shade900,
+                //       borderRadius: BorderRadius.circular(10),
+                //     ),
+                //     child: const Text(
+                //       'Sign In',
+                //       style: TextStyle(fontSize: 25, color: Colors.white),
+                //     ),
+                //   ),
+                // )
+
+                // _isSigning
+                //       ? const CircularProgressIndicator()
+                //       : ElevatedButton(
+                //           onPressed: _signIn,
                 SizedBox(
                   width: double.infinity,
-                  child: _isSigning
-                      ? CircularProgressIndicator()
-                      : ElevatedButton(
-                          onPressed: _signIn,
-                          child: Text(
-                            'Sign In',
-                            style: TextStyle(fontSize: 20, color: Colors.white),
-                          ),
-                        ),
+                  child: GestureDetector(
+                    onTap: _signIn,
+                    child: Container(
+                      padding: const EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Colors.green.shade900,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Text(
+                        'Sign In',
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      ),
+                    ),
+                  ),
                   // style: ElevatedButton.styleFrom(
                   //   padding: const EdgeInsets.symmetric(vertical: 15),
                   //   backgroundColor: Colors.green.shade900,
@@ -239,6 +197,7 @@ class NextLoginScreenState extends State<NextLoginScreen> {
                   // child: const Text(
                   //   'Sign In',
                   //   style: TextStyle(fontSize: 20, color: Colors.white),
+                  // ),
                   // ),
                 ),
               ],
