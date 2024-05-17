728f92c82338b30805b29b94fca8b494aed91440
mebcrtz <81579471+mebcrtz@users.noreply.github.com>
2024-05-17 16:21:22 +0800

    fix

--- lib/pages/profile_page/chatbot.dart
+++ lib/pages/profile_page/chatbot.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:mcl_user/components/app_bar.dart';
+// import 'package:mcl_user/components/app_bar.dart';
 
 class ChatPage extends StatefulWidget {
   static const routeName = '/chat';
